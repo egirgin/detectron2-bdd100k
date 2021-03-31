89fa3e8464e8661e488ce3846d36ed4ca0972d9e
Emre Girgin <emregirgin171@gmail.com>
2021-03-31 15:52:04 +0300

    arg parser fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-!python train.py --trainer "default" --bitmap_path "subdataset/bitmaps" --images_path "subdataset/imgs" \
+!python train.py --trainer "custom" --bitmap_path "subdataset/bitmaps" --images_path "subdataset/imgs" \
                   --epochs 10 --batch_size 4 --eval_period 2
